fc54a48d156783562f5223345050b4a7b249fc11
CJMyers1 <myerscasper73@gmail.com>
2021-11-19 12:48:19 -0700

    Updated Bookmark app

--- Bookmark-app/script.js
+++ Bookmark-app/script.js
@@ -1,1 +1,54 @@
-console.log('testing');
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEL = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmars-container');
+
+// Show Modal, Focus on Input
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEL.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+function validate(nameValue, urlValue) {
+    const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+        alert('Please Submit Values for both fields.');
+        return false;
+    }
+    if (urlValue.match(regex)) {
+        alert('match');
+    }
+    if (!urlValue.match(regex)) {
+        alert('Please provide a valid web address');
+        return false;
+    }
+    // valid
+    return true;
+}
+
+// Handle Data from Form
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEL.value;
+    let urlValue = websiteUrlEl.value;
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`;
+    }
+    console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+        return false;
+    }
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
