3d4f493800df992d83f0ad4e1d3b6f9feea42545
CJMyers1 <myerscasper73@gmail.com>
2021-11-19 08:29:56 -0700

    Music Player

--- Music-player/script.js
+++ Music-player/script.js
@@ -3,6 +3,8 @@ const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const progressContainer = document.getElementById('progress-container');
 const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
@@ -96,10 +98,37 @@ function updateProgressBar(e) {
         // Update Progress Bar Width
         const progressPercent = (currentTime / duration) * 100;
         progress.style.width = `${progressPercent}%`;
+        // Calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        // Delay switching duration Element to avoid Nan
+        if (durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // Calculate display for duration
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
+}
+
+// Set Progress Bar
+function setProgressBar(e) {
+const width = this.clientWidth;
+const clickX = e.offsetX;
+const { duration } = music;
+music.currentTime = (clickX / width) * duration;
 }
 
 // Evet Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
